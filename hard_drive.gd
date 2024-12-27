2b9dbff2ac8cea149fdbf12bf5d1d2f27c0df4d7
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2024-12-27 01:44:24 -0500

    Fix weird collision issue between HD and player, causing bits to be consumed immediately upon spawning

--- hard_drive.gd
+++ hard_drive.gd
@@ -20,7 +20,7 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 func _apply_impulse_to_body(body: RigidBody2D) -> void:
 	# Apply the impulse after the body is integrated into the physics system
 	var x_force = randi_range(-100, 100)
-	var y_force = randi_range(-100, -800)
+	var y_force = randi_range(-500, -800)
 	body.apply_impulse(Vector2(x_force, y_force), Vector2.ZERO)
 
 func _on_body_entered(body: Node2D) -> void:
