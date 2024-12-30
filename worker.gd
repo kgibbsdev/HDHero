0afdb1eb9dd300ef2e5dd5d57aca678864fc57a2
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2024-12-30 02:29:54 -0500

    Workers can now pick an existing bit as a target and move toward it. This is resilient to bits going off screen while it's chasing them

--- worker.gd
+++ worker.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 @onready var hard_drive = get_parent().get_node("HardDrive")
 var mode: worker_mode
-
+var bit_target
+var collecting_bit = false
+var move_speed = 1000
 enum worker_mode 
 {
 	collect = 1,
@@ -16,14 +18,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if mode == worker_mode.collect and position.x != hard_drive.position.x:
-		move_below_hard_drive()
+	if mode == worker_mode.collect:
+		collect_bits(delta)
+		
+		#if position.x != hard_drive.position.x and collecting_bit == false:
+			#move_below_hard_drive()
+		#else:
 		
 func move_below_hard_drive():
 	position = position.move_toward(Vector2(hard_drive.position.x, position.y), 1)
 
-
 func _on_coin_collider_body_entered(body: Node2D) -> void:
 	if body.name.to_lower().contains("bit"):
+		bit_target = null
 		body.queue_free()
 		get_parent().score_manager.add_score(1)
+
+func collect_bits(delta_time) -> void:
+	
+	if bit_target != null:
+		position = position.move_toward(Vector2(bit_target.get_global_position().x, position.y), delta_time * move_speed)
+	else:
+		bit_target = get_tree().get_first_node_in_group("bits")
