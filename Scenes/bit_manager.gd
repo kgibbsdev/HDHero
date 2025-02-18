20b1036729ffea0fee46978824be1e9f4f1cf467
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2025-02-17 21:54:34 -0500

    Bits now tell the BitManager when they go off screen and the BitManager decides whether or not they should live

--- Scenes/bit_manager.gd
+++ Scenes/bit_manager.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
 @onready var bit_scene: PackedScene = load("res://Scenes/bit.tscn")
-@export var number_of_bits = 0
 @export var bit_limit = Global.bit_limit
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,16 +9,15 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func spawn_bit(posish = Vector2(0, 0)):
-	print("doink")
+	
+func spawn_bit(spawn_position = Vector2(0, 0)):
 	if get_tree().get_nodes_in_group("bits").size() < bit_limit:
 		var bit = bit_scene.instantiate()
 		bit.get_node("AnimatedSprite2D").play()
 		bit.add_to_group("bits")
-		bit.position = to_local(posish)
+		bit.position = to_local(spawn_position)	# I don't know why this is right but it works
+		bit.get_node("VisibleOnScreenNotifier2D").connect("screen_exited", check_if_deletable.bind(bit))
 		add_child(bit, true)
-		number_of_bits += 1
 		call_deferred("_apply_impulse_to_body", bit)
 	else:
 		print("too many bits!")
@@ -29,3 +27,10 @@ func _apply_impulse_to_body(body: RigidBody2D) -> void:
 	var x_force = randi_range(-100, 100)
 	var y_force = randi_range(-500, -800)
 	body.apply_impulse(Vector2(x_force, y_force), Vector2.ZERO)
+
+func check_if_deletable(bit):
+#	Allow bits to go off of the top of the screen and come back down but die if they fall off the map
+	if bit.get_position().y < 0:
+		pass
+	else:
+		bit.queue_free(bit)
--- Scripts/bit.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody2D
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	if position.y < 0:
-		pass
-	else:
-		queue_free()
