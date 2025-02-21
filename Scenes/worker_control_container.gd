2ba8c6fc27e4855f4dab7d6b41eba8eebca8ec24
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2025-02-20 22:05:51 -0500

    Spawn worker containers as children of worker menu and space them out accordingly relative to the worker_menu

--- Scenes/worker_control_container.gd
+++ Scenes/worker_control_container.gd
@@ -1,13 +1,17 @@
 class_name WorkerControlContainer extends VFlowContainer
 
-@onready var worker_number_label: Label = get_node("WorkerNumberLabel")
+var worker_number_label: Label
 var worker_number = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	worker_number_label.text = "test"
+	worker_number_label = get_node("WorkerNumberLabel")
+	worker_number_label.text = "default_text"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_number_label(label_text):
+	worker_number_label.text = label_text
--- Scripts/worker_menu.gd
+++ Scripts/worker_menu.gd
@@ -6,15 +6,23 @@ class_name WorkerMenu extends Control
 var workers = []
 var workerCountLastFrame = 0
 var worker_controls = []
+var spacing_base_x: int = 128
+var spacing_base_y: int = 64
 func _ready() -> void:
 	pass
 	
 func add_worker_ui(worker):
 #	Will changes to the workers be reflected here as well?
 	workers.append(worker)
-	var worker_control = worker_control_container_scene.instantiate()
+	var worker_control: WorkerControlContainer = worker_control_container_scene.instantiate()
+#	Make sure to add a node to the tree before you set any of its values!
+	worker_controls.append(worker_control)
 	add_child(worker_control, true)
 
+	worker_control.set_number_label("Worker #" + str(len(workers)))
+	var spacing: Vector2 = Vector2((len(worker_controls) - 1) * spacing_base_x, position.y + spacing_base_y)
+	worker_control.set_position(spacing)
+	
 
 func _on_spawn_worker_button_pressed() -> void:
 	worker_manager.spawn_worker()
